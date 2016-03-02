eca0b7df2d5cbc1f19ecc58f65cdc360fb2e53f9
zavox <andras.kesik@gmail.com>
2016-03-02 14:27:51 +0100

    create

---
+++ src/m3u_mp3split_client/FTVL.java
@@ -0,0 +1,29 @@
+package m3u_mp3split_client;
+
+import java.io.File;
+import java.util.List;
+
+public class FTVL {
+
+    private enum Command{
+        CREATE,
+        READ,
+        SPLIT
+    }
+
+    public File createM3U(List<File> fileList){
+
+        return null;
+    }
+
+    public List<File> readM3U(File file){
+
+        return null;
+    }
+
+    public void splitMP3(){
+
+
+    }
+
+}
